c56da90b7e6f7ec2072d9d298ffc6b94420faaf3
Anirudh Jwala <anirudhsai2@gmail.com>
2022-04-18 13:19:12 +0530

    Categories screen in grid with images and text

--- screens/CategoriesScreen.tsx
+++ screens/CategoriesScreen.tsx
@@ -1,32 +1,135 @@
 import React from 'react';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Image, ImageSourcePropType } from 'react-native';
+import { FlatGrid } from 'react-native-super-grid';
 
-import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 
+interface CategoryListData {
+  key: number;
+  title: string;
+  image: ImageSourcePropType,
+  backgroundColor: string;
+  route: string;
+}
+
 export default function CategoriesScreen() {
+  const categoriesData: CategoryListData[] = [
+    {
+      key: 1,
+      title: 'Covid Essentials',
+      image: require('../assets/images/01.png'),
+      backgroundColor: '#d9e4ff',
+      route: '/',
+    },
+    {
+      key: 2,
+      title: 'Beauty & Personal Care',
+      image: require('../assets/images/02.png'),
+      backgroundColor: '#d9ffde',
+      route: '/',
+    },
+    {
+      key: 3,
+      title: 'Health Food & Beverages',
+      image: require('../assets/images/03.png'),
+      backgroundColor: '#f5ffd9',
+      route: '/',
+    },
+    {
+      key: 4,
+      title: 'Health Drinks & Supplements',
+      image: require('../assets/images/04.png'),
+      backgroundColor: '#d9ffec',
+      route: '/',
+    },
+    {
+      key: 5,
+      title: 'Home Care & Hygiene',
+      image: require('../assets/images/05.png'),
+      backgroundColor: '#efd9ff',
+      route: '/',
+    },
+    {
+      key: 6,
+      title: 'Sports',
+      image: require('../assets/images/06.png'),
+      backgroundColor: '#d9fff8',
+      route: '/',
+    },
+    {
+      key: 7,
+      title: 'Mother & Baby',
+      image: require('../assets/images/07.png'),
+      backgroundColor: '#f9d9ff',
+      route: '/',
+    },
+    {
+      key: 8,
+      title: 'Ayurvedic & Herbal',
+      image: require('../assets/images/08.png'),
+      backgroundColor: '#d9ffe3',
+      route: '/',
+    },
+    {
+      key: 9,
+      title: 'Orthopaedics',
+      image: require('../assets/images/09.png'),
+      backgroundColor: '#ffefd9',
+      route: '/',
+    },
+    {
+      key: 10,
+      title: 'Devices & Aids',
+      image: require('../assets/images/10.png'),
+      backgroundColor: '#ffe2d9',
+      route: '/',
+    },
+    {
+      key: 11,
+      title: 'School & Office Stationery',
+      image: require('../assets/images/11.png'),
+      backgroundColor: '#d9f7ff',
+      route: '/',
+    },
+  ];
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/CategoriesScreen.tsx" />
-    </View>
+    <FlatGrid
+      itemDimension={130}
+      data={categoriesData}
+      renderItem={({ item }) => (
+        <View key={item.key} style={[styles.categoryCard, { backgroundColor: item.backgroundColor, }]}>
+          <Image source={item.image} style={styles.imageContainer} />
+          <View style={styles.titleContainer}>
+            <Text style={styles.categoryTitle}>{item.title}</Text>
+          </View>
+        </View>
+      )}
+    />
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+  categoryCard: {
+    height: 180,
     alignItems: 'center',
+    justifyContent: 'space-between',
+    borderRadius: 8,
+    elevation: 1,
+  },
+  imageContainer: {
+    width: 100,
+    height: 120,
+  },
+  titleContainer: {
+    width: '100%',
+    height: 40,
+    borderBottomLeftRadius: 8,
+    borderBottomRightRadius: 8,
     justifyContent: 'center',
   },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+  categoryTitle: {
+    fontSize: 15,
+    textAlign: 'center',
   },
 });
--- screens/NotificationScreen.tsx
+++ screens/NotificationScreen.tsx
@@ -2,15 +2,17 @@ import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { Platform, StyleSheet } from 'react-native';
 
-import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 
 export default function NotificationScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Notification</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/NotificationScreen.tsx" />
+      <Text
+        style={styles.noNotifications}
+        lightColor="rgba(0,0,0,0.8)"
+        darkColor="rgba(255,255,255,0.8)">
+        No Notifications at the moment
+      </Text>
 
       <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
     </View>
@@ -21,15 +23,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
   },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+  noNotifications: {
+    fontSize: 16,
+    marginTop: 32,
+    textAlign: 'center',
   },
 });
